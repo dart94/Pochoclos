aa810b6bf13c23868e5f49a951d5ad41bf27b84a
dart94 <167586944+dart94@users.noreply.github.com>
2024-07-22 17:49:52 -0700

    sett

--- app.py
+++ app.py
@@ -7,19 +7,15 @@ load_dotenv()  # Carga las variables de entorno desde '.env'
 
 app = Flask(__name__)
 
-# Obtiene la API key de las variables de entorno
-API_KEY = os.getenv('API_KEY')
-
+API_KEY = os.getenv('API_KEY')  # Obtiene la API key de las variables de entorno
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/rated_movies')
 def rated_movies():
-    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={
-        API_KEY}&language=es-ES"
+    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=es-ES"
     response = requests.get(url)
     image_base_url = "https://image.tmdb.org/t/p/w500"
     if response.status_code == 200:
@@ -34,19 +30,16 @@ def rated_movies():
     else:
         return '<div>No se encontraron películas calificadas.</div>'
 
-
 @app.route('/rated_tv')
 def rated_tv():
-    url = f"https://api.themoviedb.org/3/tv/top_rated?api_key={
-        API_KEY}&language=es-ES"
+    url = f"https://api.themoviedb.org/3/tv/top_rated?api_key={API_KEY}&language=es-ES"
     response = requests.get(url)
     image_base_url = "https://image.tmdb.org/t/p/w500"
     if response.status_code == 200:
         tv_shows = response.json().get('results', [])
         tv_shows_html = ''.join([
             f"<div class='movie-item'>"
-            f"<img src='{image_base_url}{
-                tv_show['poster_path']}' alt='Poster'>"
+            f"<img src='{image_base_url}{tv_show['poster_path']}' alt='Poster'>"
             f"<a href='/tv_details/{tv_show['id']}'>{tv_show['name']}</a> - "
             f"Calificación: {tv_show['vote_average']}</div>"
             for tv_show in tv_shows if tv_show['poster_path']])
@@ -54,38 +47,30 @@ def rated_tv():
     else:
         return '<div>No se encontraron series de TV calificadas.</div>'
 
-
 @app.route('/search')
 def search():
     query = request.args.get('query')
-    movie_url = f"https://api.themoviedb.org/3/search/movie?api_key={
-        API_KEY}&language=es-ES&query={query}"
-    tv_url = f"https://api.themoviedb.org/3/search/tv?api_key={
-        API_KEY}&language=es-ES&query={query}"
+    movie_url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&language=es-ES&query={query}"
+    tv_url = f"https://api.themoviedb.org/3/search/tv?api_key={API_KEY}&language=es-ES&query={query}"
 
     movie_response = requests.get(movie_url)
-    movies = movie_response.json().get(
-        'results', []) if movie_response.status_code == 200 else []
+    movies = movie_response.json().get('results', []) if movie_response.status_code == 200 else []
     tv_response = requests.get(tv_url)
-    tv_shows = tv_response.json().get(
-        'results', []) if tv_response.status_code == 200 else []
+    tv_shows = tv_response.json().get('results', []) if tv_response.status_code == 200 else []
 
     image_base_url = "https://image.tmdb.org/t/p/w500"
 
     index_html = ''.join([
-        f"<div class='movie-item'><img src='{image_base_url}{
-            movie['poster_path']}' alt='Poster' style='height:100px;'>"
+        f"<div class='movie-item'><img src='{image_base_url}{movie['poster_path']}' alt='Poster' style='height:100px;'>"
         f"{movie['title']} (Película)</div>"
         for movie in movies if movie['poster_path']])
 
     index_html += ''.join([
-        f"<div class='movie-item'><img src='{image_base_url}{
-            tv['poster_path']}' alt='Poster' style='height:100px;'>"
+        f"<div class='movie-item'><img src='{image_base_url}{tv['poster_path']}' alt='Poster' style='height:100px;'>"
         f"{tv['name']} (TV)</div>"
         for tv in tv_shows if tv['poster_path']])
 
     return index_html if index_html else '<div>No se encontraron resultados.</div>'
-
 
 @app.route('/review/<review_id>')
 def review_details(review_id):
@@ -99,25 +84,20 @@ def review_details(review_id):
     else:
         return '<div>Review not found.</div>'
 
-
 @app.route('/reviews/<media_type>/<int:media_id>')
 def reviews(media_type, media_id):
-    url = f"https://api.themoviedb.org/3/{media_type}/{
-        media_id}/reviews?api_key={API_KEY}&language=es-ES"
+    url = f"https://api.themoviedb.org/3/{media_type}/{media_id}/reviews?api_key={API_KEY}&language=es-ES"
     response = requests.get(url)
     if response.status_code == 200:
         reviews = response.json().get('results', [])
-        reviews_html = ''.join([f"<div><strong>{
-                               review['author']}</strong>: {review['content']}</div>" for review in reviews])
+        reviews_html = ''.join([f"<div><strong>{review['author']}</strong>: {review['content']}</div>" for review in reviews])
         return reviews_html if reviews else '<div>No se encontraron reseñas.</div>'
     else:
         return '<div>Error al cargar reseñas.</div>'
 
-
 @app.route('/popular-movies')
 def popular_movies():
-    url = f"https://api.themoviedb.org/3/movie/popular?api_key={
-        API_KEY}&language=es-ES"
+    url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&language=es-ES"
     response = requests.get(url)
     image_base_url = "https://image.tmdb.org/t/p/w500"
     if response.status_code == 200:
@@ -132,44 +112,35 @@ def popular_movies():
     else:
         return '<div>No se encontraron películas calificadas.</div>'
 
-
 @app.route('/movie_details/<int:movie_id>')
 def movie_details(movie_id):
-    movie_url = f"https://api.themoviedb.org/3/movie/{
-        movie_id}?api_key={API_KEY}&language=es-ES"
-    videos_url = f"https://api.themoviedb.org/3/movie/{
-        movie_id}/videos?api_key={API_KEY}&language=es-ES"
+    movie_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=es-ES"
+    videos_url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={API_KEY}&language=es-ES"
 
     movie_response = requests.get(movie_url)
     videos_response = requests.get(videos_url)
 
     if movie_response.status_code == 200:
         movie_details = movie_response.json()
-        videos = videos_response.json(
-        )['results'] if videos_response.status_code == 200 else []
+        videos = videos_response.json()['results'] if videos_response.status_code == 200 else []
         return render_template('movie_details.html', details=movie_details, videos=videos)
     else:
         return '<div>Error al obtener los detalles de la película.</div>'
 
-
 @app.route('/tv_details/<int:tv_id>')
 def tv_details(tv_id):
-    tv_url = f"https://api.themoviedb.org/3/tv/{
-        tv_id}?api_key={API_KEY}&language=es-ES"
-    videos_url = f"https://api.themoviedb.org/3/tv/{
-        tv_id}/videos?api_key={API_KEY}&language=es-ES"
+    tv_url = f"https://api.themoviedb.org/3/tv/{tv_id}?api_key={API_KEY}&language=es-ES"
+    videos_url = f"https://api.themoviedb.org/3/tv/{tv_id}/videos?api_key={API_KEY}&language=es-ES"
 
     tv_response = requests.get(tv_url)
     videos_response = requests.get(videos_url)
 
     if tv_response.status_code == 200:
         tv_details = tv_response.json()
-        videos = videos_response.json(
-        )['results'] if videos_response.status_code == 200 else []
+        videos = videos_response.json()['results'] if videos_response.status_code == 200 else []
         return render_template('tv_details.html', details=tv_details, videos=videos)
     else:
         return '<div>Error al obtener los detalles de la serie de TV.</div>'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
